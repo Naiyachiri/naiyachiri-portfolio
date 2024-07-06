428d95f3124311bdd3c83ca167a9db4128ea94a7
Naiyachiri <winddemonx519@hotmail.com>
2024-07-06 08:32:26 -0400

    Adds react router
    - converts application is a npm app instead of
    yarn

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { getContacts } from "./contacts"
+
+import "./index.css";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact"
+import Root from './routes/root'
+
+export async function rootLoader() {
+  const contacts = await getContacts()
+  return { contacts }
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
+  },
+], { basename: "/naiyachiri-portfolio" });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
